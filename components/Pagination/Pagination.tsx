4b0bf4a957622db8a9f6c87deef2d775f610c17b
valeriy <portmone1@gmail.com>
2026-06-16 20:02:14 +0300

    fix Pagination

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -1,67 +1,16 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { useQuery } from '@tanstack/react-query';
-import { fetchNotes } from '@/lib/api/clientApi';
-import type { NoteTag } from '@/types/note';
-import SearchBox from '@/components/SearchBox/SearchBox';
-import Pagination from '@/components/Pagination/Pagination';
-import NoteList from '@/components/NoteList/NoteList';
-import css from './NotesPage.module.css';
+import css from './Pagination.module.css';
 
-interface NotesClientProps {
-  tag?: NoteTag;
+interface PaginationProps {
+  page: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
 }
 
-export default function NotesClient({ tag }: NotesClientProps) {
-  const [page, setPage] = useState(1);
-  const [search, setSearch] = useState('');
-  const [debouncedSearch, setDebouncedSearch] = useState('');
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setDebouncedSearch(search);
-      setPage(1);
-    }, 500);
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [search]);
-
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['notes', page, debouncedSearch, tag],
-    queryFn: () =>
-      fetchNotes({
-        page,
-        search: debouncedSearch,
-        tag,
-      }),
-  });
-
-  return (
-    <main className={css.app}>
-      <div className={css.toolbar}>
-        <SearchBox value={search} onChange={setSearch} />
-
-        {data && data.totalPages > 1 && (
-          <Pagination
-            page={page}
-            totalPages={data.totalPages}
-            onPageChange={setPage}
-          />
-        )}
-
-        <Link href="/notes/action/create" className={css.button}>
-          Create note +
-        </Link>
-      </div>
-
-      {isLoading && <p>Loading notes...</p>}
-      {isError && <p>Something went wrong.</p>}
-      {data && data.notes.length > 0 && <NoteList notes={data.notes} />}
-      {data && data.notes.length === 0 && <p>No notes found.</p>}
-    </main>
-  );
-}
+export default function Pagination({
+  page,
+  totalPages,
+  onPageChange,
+}: PaginationProps) {
+  const handle
