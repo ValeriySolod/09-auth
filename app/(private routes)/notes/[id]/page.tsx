90de7b636fe33009344509cb5e650e246f634dd5
valeriy <portmone1@gmail.com>
2026-06-16 19:33:02 +0300

    fix: remove old api imports

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
   dehydrate,
 } from '@tanstack/react-query';
-import { fetchNoteById } from '@/lib/api/notes';
+import { fetchNoteById } from '@/lib/api/serverApi';
 import NoteDetailsClient from './NoteDetails.client';
 
 interface NoteDetailsPageProps {
