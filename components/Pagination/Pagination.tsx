a17c7ebd17ae191c2f0f95cee6260eb42becba92
valeriy <portmone1@gmail.com>
2026-06-16 20:13:31 +0300

    clientApi

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -13,4 +13,45 @@ export default function Pagination({
   totalPages,
   onPageChange,
 }: PaginationProps) {
-  const handle
+  const handlePrev = () => {
+    if (page > 1) {
+      onPageChange(page - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (page < totalPages) {
+      onPageChange(page + 1);
+    }
+  };
+
+  if (totalPages <= 1) {
+    return null;
+  }
+
+  return (
+    <div className={css.pagination}>
+      <button
+        className={css.button}
+        onClick={handlePrev}
+        disabled={page === 1}
+        type="button"
+      >
+        Prev
+      </button>
+
+      <span className={css.text}>
+        Page {page} of {totalPages}
+      </span>
+
+      <button
+        className={css.button}
+        onClick={handleNext}
+        disabled={page === totalPages}
+        type="button"
+      >
+        Next
+      </button>
+    </div>
+  );
+}
